7624258462e8fb23b69afd4c788595bd5d7fb8c5
Mhandisi <engsephy@gmail.com>
2022-09-08 23:26:48 +0300

    Solved the removeFromArray and leapYears exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,22 @@
-const removeFromArray = function(array) {
-   array.forEach(element => {
-        array.unshift(element)
-    });
-};
+const removeFromArray = function(ari,...elementsToRemove) {
+         /*rest operator (...) has been used to organize the parameters
+          into an array called elementsToRemove for easy handling*/
+        let forRemoval = [];
+        for (let i = 0; i < elementsToRemove.length; i++) {
+            //getting the index of the elements to be removed
+            const toRemove = ari.indexOf(elementsToRemove[i]);
+            //putting the indices of the elements to be removed into another array
+            forRemoval.push(toRemove)
+        }
+        //A set is a collection of items which are unique & is declare as below.
+        //A set can hold any value of any data types e.g the unique indices
+        const indexSet = new Set(forRemoval);
+        //Only elements which are not in the indexSet are returned
+        const arrayWithValuesRemoved = ari.filter((value, i) => !indexSet.has(i));
+
+        return arrayWithValuesRemoved;
+    
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(n) {
+    //not every value divisible by 4 such as 1900 is a leap year
+    //if last two digits !==00 && divisible by 4 = leap year
+    //if last two digits == 00 && !n%100 == 0 && n%400==0 
+    //getting the last two digits
+    //converting the year to string and then gettig the last two characters
+    let lastTwoDigits = n.toString().slice(-2);
 
+    if ((lastTwoDigits == "00") && (!n%100==0) && (n%400 ==0)) {
+        return true;
+    }else if((lastTwoDigits !== "00") && (n%4==0)){
+        return true;
+    }else{
+        return false;
+    }
 };
-
 // Do not edit below this line
 module.exports = leapYears;
