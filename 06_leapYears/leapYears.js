624ab1071b5056b35c3c18cb42f58d0c22217d66
Mhandisi <engsephy@gmail.com>
2022-09-09 07:50:32 +0300

    All tests from leapYears and tempConversion exercices passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,8 @@
 const leapYears = function(n) {
-    //not every value divisible by 4 such as 1900 is a leap year
-    //if last two digits !==00 && divisible by 4 = leap year
-    //if last two digits == 00 && !n%100 == 0 && n%400==0 
-    //getting the last two digits
     //converting the year to string and then gettig the last two characters
+    /*The slice() method extracts a section of a string and returns it as a new string,
+    without modifying the original string. On using a -ve sign i.e slice(-n) elements
+    from the last end will be returned*/
     let lastTwoDigits = n.toString().slice(-2);
 
     if ((lastTwoDigits == "00") && (!n%100==0) && (n%400 ==0)) {
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+//[°C] = ([°F] − 32) × 5⁄9
+//f = temp in Fahrenheit to be converted to celcius
+let toCelcius = (f - 32) * (5/9);
+let result = Math.round(toCelcius*10)/10;
+return result;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+//[°F] = [°C] × 9⁄5 + 32
+//c = temp in Celcius to be converted to Fahrenheit
+let toFahrenheit = c * (9/5) + 32;
+let result = Math.round(toFahrenheit*10)/10;
+return result;
 };
 
 // Do not edit below this line
