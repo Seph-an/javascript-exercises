e1a1df893da41ab5605bb716c44736d27e84d12f
Mhandisi <engsephy@gmail.com>
2022-09-09 08:53:14 +0300

    All calculator tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(x,y) {
+  let sum = 0;
+      sum = x+y;
+      return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(m,n) {
+	const sub = m-n;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  let sum = 0;
+	for (let i = 0; i < x.length; i++) {
+    const element = x[i];
+    sum += element;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  let product = 1;
+	for (let i = 0; i < x.length; i++) {
+    const element = x[i];
+    product *= element;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(n,x) {
+  const power = Math.pow(n,x);
+	return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  //recursion has been employed to find solution
+	if (x==1 || x==0) {
+    return 1;
+  } else {
+    return x * factorial (x-1);
+  }
 };
 
 // Do not edit below this line
