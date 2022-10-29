a48db3459b742836be7c6ac9e95a6c6cc81baf07
Mhandisi <engsephy@gmail.com>
2022-10-29 09:56:05 +0300

    Palinrome checker passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (param) {
+  let str = param;
+  //{/[^a-zA-Z ]/g, ""} - strips sentence off all punctuations
+  //g - ensures that all matches are shown t& not just he first one
 
+  let s1 = str
+    .replace(/[^a-zA-Z ]/g, "")
+    .toLowerCase()
+    .split(" ")
+    .join("");
+
+  let s2 = s1.split("").reverse().join("");
+
+  if (s1 == s2) {
+    // console.log("its a palindrome");
+    return true;
+  } else {
+    // console.log("its not a palindrome");
+    return false;
+  }
 };
-
+// palindromes(a);
 // Do not edit below this line
 module.exports = palindromes;
